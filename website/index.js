d7170d7e43878c02e6cf424918e9f85d17c4aea1
tselentis-sp <tselentis.sp@gmail.com>
2023-12-26 23:37:25 +0200

    views change

--- website/index.js
+++ website/index.js
@@ -1,8 +1,9 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://coy2bha4ejncqc7vm7usd24qcy0illix.lambda-url.eu-central-1.on.aws/");
+    let response = await fetch(
+        "https://coy2bha4ejncqc7vm7usd24qcy0illix.lambda-url.eu-central-1.on.aws/"
+    );
     let data = await response.json();
-    counter.innerHTML = 'views: ${data}';
+    counter.innerHTML = `👀 Views: ${data}`;
 }
-
 updateCounter();
